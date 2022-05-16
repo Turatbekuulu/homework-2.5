e2144c712ba42ffe1092375eb15cf7eaaadceece
Arsen <turatbekuuluarsen@gmail.com>
2022-05-17 01:13:20 +0600

    Updated

--- Phone/main.swift
+++ Phone/main.swift
@@ -1,11 +1,10 @@
 var iPhone13 = iPhone(name: "iPhone 13", memory: 256, yearMade: 2021, color: "Green", condition: "New", display: "Super Retina XDR display")
-iPhone13.showExternalCharacteristics()
-iPhone13.showTechincalCharacteristics()
-print("-----------------------")
 var samsungS21 = Samsung(name: "Samsung galaxy s21", memory: 256, yearMade: 2021, color: "Black", condition: "New", display: "Dynamic AMOLED 2X")
-samsungS21.showExternalCharacteristics()
-samsungS21.showTechincalCharacteristics()
-print("-----------------------")
 var huaweiP40Pro = Huawei(name: "Huawei P40 Pro", memory: 128, yearMade: 2020, color: "Blue", condition: "New", display: "OLED")
-huaweiP40Pro.showExternalCharacteristics()
-huaweiP40Pro.showTechincalCharacteristics()
+
+var phones: [Phone] = [iPhone13, samsungS21, huaweiP40Pro]
+
+for phone in phones {
+    phone.showExternalCharacteristics()
+    phone.showTechincalCharacteristics()
+}
